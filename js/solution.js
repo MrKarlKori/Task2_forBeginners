d67d4885f65678fc169fee1b8ad8b9e7ad229d1b
karlkori <skaller@tut.by>
2018-01-30 21:59:24 +0300

    added solution

--- js/solution.js
+++ js/solution.js
@@ -11,8 +11,58 @@
      */
     function solution(map) {
         // todo: подсчитать кол-во островов на карте
-        return 0;
+        let islands = [],
+            visited = [],
+            lastCell = null;
+
+        for (let row = 0; row < map.length; row++) {
+
+            for (let cell = 0; cell < map[row].length; cell++) {
+                
+                if ( !map[row][cell] || cellVisited(`${row}${cell}`) ) {
+                    continue;
+                } else {
+                    islands.push([`${row}${cell}`]);
+                    visited.push(`${row}${cell}`);
+
+                    addCellsToIsland(row, cell, "new");
+                }
+
+                function addCellsToIsland(row, cell, src) {
+                    if ( row > 0 && !cellVisited(`${row-1}${cell}`) && map[row-1][cell] ) {
+                        addNewCell(`${row-1}${cell}`);
+                    }
+                    if ( row < map.length-1 && !cellVisited(`${row+1}${cell}`) && map[row+1][cell] ) {
+                        addNewCell(`${row+1}${cell}`);
+                    }
+                    if ( cell > 0 && !cellVisited(`${row}${cell-1}`) && map[row][cell-1] ) {
+                        addNewCell(`${row}${cell-1}`);
+                    }
+                    if ( cell < map[row].length-1 && !cellVisited(`${row}${cell+1}`) && map[row][cell+1] ) {
+                        addNewCell(`${row}${cell+1}`);
+                    }
+                }
+
+                function cellVisited(check) {
+                    if ( visited.includes(check) ) {
+                        return true;
+                    }
+                    return false;
+                }
+
+                function addNewCell(item) {
+                    islands[islands.length-1].push(item);
+                    visited.push(item);
+
+                    addCellsToIsland(+item[0], +item[1], "old");
+                }
+            }
+
+        }
+        console.log(islands);
+        return islands.length;
     }
+
 
     root.SHRI_ISLANDS.solution = solution;
 })(this);
